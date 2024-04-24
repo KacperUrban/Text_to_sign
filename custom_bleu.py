94737191553e80bdfde651deb9f355ff1d55a7e5
Kacper Urban <urbankacper613@gmail.com>
2024-04-24 11:55:45 +0200

    Update custombleu to operate only ona DatasetDict

--- custom_bleu.py
+++ custom_bleu.py
@@ -6,16 +6,16 @@ from typing import Tuple
 from datasets import DatasetDict
 import numpy as np
 
-#Tu wystarczy podać tylko DatasetDict i iterować po train valid i test  i tworzyć hashmape
+
 class CustomBleu:
     """That class is modified version of common functin BLEU from nltk library. In this class was added
     preprocessing phase on data and operate on DatasetDict from datasets library.
     """
-    def __init__(self, data:DataFrame, translator:pipeline) -> None:
+    def __init__(self, data:DatasetDict, translator:pipeline) -> None:
         """Init function
 
         Args:
-            data (DataFrame): Dataframe with all examples
+            data (DatasetDict): DatasetDict with all examples.
             translator (pipeline): Transformers pipeline
         """
         self.data = data
@@ -28,30 +28,29 @@ class CustomBleu:
             dict: Return dictionary with sentence in base language and its translation sentences.
         """
         hashmap = {}
-        for index, row in self.data.iterrows():
-            polish_sentence = row['pl']
-            sign_language_sentence = row['mig']
-            
-            if polish_sentence in hashmap:
-                hashmap[polish_sentence].append(sign_language_sentence)
-            else:
-                hashmap[polish_sentence] = [sign_language_sentence]
+        
+        for dataset in ['train', 'valid', 'test']:
+            for i in range(len(self.data[dataset])):
+                polish_sentence = self.data[dataset][i]['translation']['pl']
+                sign_language_sentence = self.data[dataset][i]['translation']['mig']
+                if polish_sentence in hashmap:
+                    hashmap[polish_sentence].append(sign_language_sentence)
+                else:
+                    hashmap[polish_sentence] = [sign_language_sentence]
         return hashmap
 
-    def score(self, dataset:DatasetDict) -> Tuple[float, DataFrame]:
+    def score(self) -> Tuple[float, DataFrame]:
         """Build a hashmap of sentences and its translations. The calculate BLEU score.
-        Args:
-            dataset (DatasetDict): _description_
-
+        
         Returns:
             Tuple[float, DataFrame]: Return BLEU score and DataFrame with references and translated sentences
         """
         ref_sent = self.prepare_label_to_bleu()
         translation_corpus = []
         reference_corpus = []
-        for i in range(0, len(dataset["test"]["translation"])):
-            value = ref_sent[dataset["test"]["translation"][i]["pl"]]
-            translation = self.translator(dataset["test"]["translation"][i]["pl"])
+        for i in range(0, len(self.data["test"]["translation"])):
+            value = ref_sent[self.data["test"]["translation"][i]["pl"]]
+            translation = self.translator(self.data["test"]["translation"][i]["pl"])
             translation_corpus.append(translation[0]["translation_text"].split())
             if len(value) > 1:
                 reference_corpus.append([j.split() for j in value])
