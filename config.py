b6726af7675af3c37ea225b2a13e37362d9d3d1a
Kacper Urban <urbankacper613@gmail.com>
2025-04-14 13:50:41 +0200

    refactor: add function to load, save and check new model and tokenizer (removed redundancy in code)

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 #Define custom paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
-MODEL_DIR = os.path.join(BASE_DIR, "model", "model")
-FINAL_MODEL_DIR = os.path.join(BASE_DIR, "model", "final_model")
-TOKENIZER_DIR = os.path.join(BASE_DIR, "model", "tokenizer","")
-TOKENIZER_DE_DIR = os.path.join(BASE_DIR, "model", "tokenizer_de", "")
+MODEL_DIR = os.path.join(BASE_DIR, "models", "model")
+FINAL_MODEL_DIR = os.path.join(BASE_DIR, "models", "final_model")
+TOKENIZER_DIR = os.path.join(BASE_DIR, "models", "tokenizer","")
+TOKENIZER_DE_DIR = os.path.join(BASE_DIR, "models", "tokenizer_de", "")
 CUSTOM_UTILS_DIR = os.path.join(BASE_DIR, "custom_utils")
--- scripts/load_model.py
+++ scripts/load_model.py
@@ -5,38 +5,42 @@ parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
 
 
-from config import MODEL_DIR, TOKENIZER_DIR
+from config import MODEL_DIR, TOKENIZER_DIR, TOKENIZER_DE_DIR
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
 
+def load_save_check_model(PATH_R: str, PATH_M_F: str, PATH_T_F: str, sentence_to_translation: str) -> None:
+    """This function load model and tokenizer from remote HuggingFace repository. Then save model to the local folder
+    and do translation on specified sentence. The process have to ensure that base model was properly fetched and saved.
+
+    Args:
+        PATH_R (str): path to remote model project (e. g. Helsinki-NLP/opus-mt-pl-en)
+        PATH_M_F (str): path where will be model stored
+        PATH_T_F (str): path where will be tokenizer stored
+        sentence_to_translation (str): as name says this sentence wil be translated
+    """
+    print("Model and tokenizer are loading...")
+    tokenizer = AutoTokenizer.from_pretrained(PATH_R)
+    model = AutoModelForSeq2SeqLM.from_pretrained(PATH_R)
+
+    tokenizer.save_pretrained(PATH_T_F)
+    model.save_pretrained(PATH_M_F, safe_serialization=True)
+
+    print(f"Filepath to tokenizer: {PATH_T_F}")
+    print(f"Filepath to model: {PATH_M_F}")
+    print("Succesfully saved!")
+
+    tokenizer = AutoTokenizer.from_pretrained(PATH_T_F)
+    model = AutoModelForSeq2SeqLM.from_pretrained(PATH_M_F)
+
+    print("First translation:")
+    translation = pipeline("translation", model=model, tokenizer=tokenizer)
+    print(translation(sentence_to_translation))
+
+
 
 if __name__ == "__main__":
-    # load and save model
-    tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-pl-en")
-    model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-pl-en")
 
-    tokenizer_de = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-de-en")
-    model_de = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-de-en")
-
-    print("Model and tokenizer are loading...")
-
-    tokenizer.save_pretrained(TOKENIZER_DIR)
-    model.save_pretrained(MODEL_DIR, safe_serialization=True)
-
-    tokenizer_de.save_pretrained(TOKENIZER_DIR + "_de")
-    model_de.save_pretrained(MODEL_DIR + "_de", safe_serialization=True)
-
-    print("Succesfull saving!")
-
-    model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR)
-    tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_DIR)
-
-    model_de = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR + "_de")
-    tokenizer_de = AutoTokenizer.from_pretrained(TOKENIZER_DIR + "_de")
-    print("First translation:")
-
-    translation = pipeline("translation", model=model, tokenizer=tokenizer)
-    print(translation("Lubie jesc jablka!"))
-
-    translation_de = pipeline("translation", model=model_de, tokenizer=tokenizer_de)
-    print(translation_de("Ich esse gerne Äpfel!"))
+    PATHS = {"pl" : "Helsinki-NLP/opus-mt-pl-en", "de" : "Helsinki-NLP/opus-mt-de-en"}
+    load_save_check_model(PATHS["pl"], MODEL_DIR, TOKENIZER_DIR, "Lubie jesc jablka!")
+    load_save_check_model(PATHS["de"], MODEL_DIR + "_de", TOKENIZER_DE_DIR, "Ich esse gerne Äpfel!")
